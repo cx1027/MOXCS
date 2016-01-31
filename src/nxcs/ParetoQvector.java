bb8a47e8cbc57b4ecdb9fac0fa8384a1e38eddb4
Mallegrissimo <nuannuan7@gmail.com>
2016-02-01 10:55:46 +1300

    only deep clone candidate classifier to avoid overhead

--- src/nxcs/ParetoQvector.java
+++ src/nxcs/ParetoQvector.java
@@ -6,11 +6,10 @@ import java.util.List;
 import com.rits.cloning.Cloner;
 
 public class ParetoQvector {
-    private Cloner cloner; 
-    
+	private Cloner cloner;
+
 	private List<ArrayList<Double>> candidatelist;
-	
-	
+
 	public ParetoQvector() {
 		this.cloner = new Cloner();
 	}
@@ -19,24 +18,24 @@ public class ParetoQvector {
 		ArrayList<Qvector> archivinglist = new ArrayList<Qvector>();
 		try {
 			// TODO: how to add element
-			Qvector c0 = currParentoCandidate.get(0);//get first item from candidatelist
-			archivinglist.add(c0);//give first item to archivinglist
+			Qvector c0 = currParentoCandidate.get(0);// get first item from
+														// candidatelist
+			archivinglist.add(c0);// give first item to archivinglist
 
 			// archivinglist.set(0, c0);
 			int result;
 			int ci = 0;
-			
-			
-			for (int i=1; i< currParentoCandidate.size();i++) {
-//				if (ci == 0) {
-//					ci = 1;// c0 is already in archivinglist
-//					continue;
-//				}
-				boolean flag = true;
-//				ActionPareto candidate = currParentoCandidate.get(i);//
-				Qvector candidate = currParentoCandidate.get(i);
 
-				for (int j =0; j<archivinglist.size();j++) {
+			for (int i = 1; i < currParentoCandidate.size(); i++) {
+				// if (ci == 0) {
+				// ci = 1;// c0 is already in archivinglist
+				// continue;
+				// }
+				boolean flag = true;
+				// ActionPareto candidate = currParentoCandidate.get(i);//
+				Qvector candidate = cloner.deepClone(currParentoCandidate.get(i));
+
+				for (int j = 0; j < archivinglist.size(); j++) {
 					Qvector archiving = archivinglist.get(j);
 					result = Dominate(candidate, archiving);
 					if (result == 2) {// candidate is non donminate
@@ -49,7 +48,7 @@ public class ParetoQvector {
 					}
 				}
 				if (flag == true) {
-					archivinglist.add(cloner.deepClone(candidate));
+					archivinglist.add(candidate);
 				}
 			}
 
@@ -66,8 +65,7 @@ public class ParetoQvector {
 			result = 1;
 		} else if (candidate.get(0) >= archiving.get(0) && candidate.get(1) >= archiving.get(1)) {
 			result = 2;
-		} else if (candidate.get(0) <= archiving.get(0)
-				&& candidate.get(1) <= archiving.get(1)) {
+		} else if (candidate.get(0) <= archiving.get(0) && candidate.get(1) <= archiving.get(1)) {
 			result = 3;
 		}
 		return result;
